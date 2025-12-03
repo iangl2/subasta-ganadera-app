aeaf86a6d21e9ab1b5732885e8b120ba791aded3
RAGG1911 <raul1911.g@gmail.com>
2025-12-03 18:22:15 -0500

    feat: update auction page to include animal details and improve countdown functionality

--- resources/views/auction.blade.php
+++ resources/views/auction.blade.php
@@ -1,150 +1,171 @@
-   
-   @extends('layouts.app')
+ @extends('layouts.app')
 
 @section('title', 'Mercado')
 
 @section('content')
-       @vite(['resources/css/auction_styles.css', 'resources/js/app.js'])
-   <main class="auction_details">
-        <!-- imagen de las vaquitas -->
-         <div class="animal_details">
-            <img class="moo_picture" src="/img/Vacas-lecheras.png" alt="Subasta de Vacas Lecheras en Rancho El Buen Pastor"/>
-            <table class ="animal_table">
-                <thead>
-                    <tr>                    
-                        <th>Sexo</th>
-                        <th>Raza</th>
-                        <th>Peso</th>
-                        <th>Vendedor</th>
-                        <th>Lugar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>                  
-            </table>            
+@vite(['resources/css/auction_styles.css', 'resources/js/app.js', 'resources/js/countdown.js'])
+
+<main class="auction_details">
+
+    <!-- DATOS DEL ANIMAL -->
+    <div class="animal_details">
+
+        <img class="moo_picture" src="/img/Vacas-lecheras.png" alt="Subasta de Vacas Lecheras"/>
+
+        <table class="animal_table">
+            <thead>
+                <tr>
+                    <th>Sexo</th>
+                    <th>Raza</th>
+                    <th>Peso</th>
+                    <th>Vendedor</th>
+                    <th>Lugar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ $auction->cow->sex }}</td>
+                    <td>{{ $auction->cow->breed }}</td>
+                    <td>{{ $auction->cow->weight }} kg</td>
+                    <td>{{ $auction->seller->name }}</td>
+                    <td>{{ $auction->location }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- DETALLES DE LA SUBASTA -->
+    <div class="details">
+
+        <div class="auction_name">
+            <h1>{{ $auction->name }}</h1>
+            <p>{{ $auction->description }}</p>
         </div>
-         <!-- detalles de la subasta -->
-         <div class="details">
-            <div class="auction_name">
-                <h1>Subasta de Vacas Lecheras</h1>
-                <p>Descripción</p>                
+
+        <span>Termina en</span>
+
+        <!-- COUNTDOWN -->
+        <div class="countdown" data-end="{{ $auction->end_date }}">
+
+            {{-- Mantengo tu estructura original de countdown --}}
+            <div class="time-section" id="days">
+                <div class="time-group">
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <span>Termina en</span>
-            <div class="countdown">
-                
-                <div class="time-section" id="days">
-                    <div class="time-group">
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
+
+            {{-- HOURS --}}
+            <div class="time-section" id="hours">
+                <div class="time-group">
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
                             </div>
                         </div>
                     </div>
-                </div> 
-                <div class="time-section" id="hours">
-                    <div class="time-group">
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
                             </div>
                         </div>
                     </div>
-                </div> 
-                <div class="time-section" id="minutes">
-                    <div class="time-group">
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-                <div class="time-section" id="seconds">
-                    <div class="time-group">
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="time-segment">
-                            <div class="segment-display">
-                                <div class="segment-display-top">0</div>
-                                <div class="segment-display-bottom">0</div>
-                                <div class="segment-overlay">
-                                    <div class="segment-overlay-top">0</div>
-                                    <div class="segment-overlay-bottom">0</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>                
-            </div>            
-            <div class="price">
-                <span>Puja más reciente</span>
-                <h2>$1,200</h2> 
+                </div>
             </div>
-            <div class="bid">
-                <input type="number" id="bid_amount" name="bid_amount" placeholder="Ingresa tu puja">
-                <a class="btn_pujar" href="#">Pujar Ahora</a> 
+
+            {{-- MINUTES --}}
+            <div class="time-section" id="minutes">
+                <div class="time-group">
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
-         </div>       
-    </main>
-    @endsection
+
+            {{-- SECONDS --}}
+            <div class="time-section" id="seconds">
+                <div class="time-group">
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="time-segment">
+                        <div class="segment-display">
+                            <div class="segment-display-top">0</div>
+                            <div class="segment-display-bottom">0</div>
+                            <div class="segment-overlay">
+                                <div class="segment-overlay-top">0</div>
+                                <div class="segment-overlay-bottom">0</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- PRECIO -->
+        <div class="price">
+            <span>Puja más reciente</span>
+            <h2>${{ $auction->highest_bid ?? $auction->starting_price }}</h2> 
+        </div>
+
+        <!-- INPUT DE PUJA -->
+        <div class="bid">
+            <input type="number" id="bid_amount" name="bid_amount" placeholder="Ingresa tu puja">
+            <a class="btn_pujar" href="#">Pujar Ahora</a> 
+        </div>
+
+    </div>
+
+</main>
+@endsection
